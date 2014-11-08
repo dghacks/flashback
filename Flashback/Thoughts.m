09f63fdb10da0066073d637f5e21449c16ff56f9
HiDaN4 <hidan446@gmail.com>
2014-11-08 07:48:21 -0800

    Create CoreData stack

---
+++ Flashback/Thoughts.m
@@ -0,0 +1,17 @@
+//
+//  Thoughts.m
+//  Flashback
+//
+//  Created by Dmitry Sokolov on 11/8/14.
+//  Copyright (c) 2014 Sputnik Labs. All rights reserved.
+//
+
+#import "Thoughts.h"
+
+
+@implementation Thoughts
+
+@dynamic thought;
+@dynamic date;
+
+@end
